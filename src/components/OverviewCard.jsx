cbf51885a8cf1f1741f246e4dca51e6a6b7fea73
luisAngel <hluisangel96@gmail.com>
2026-09-16 21:40:11 -0600

    agregamos cards con data json

--- src/components/OverviewCard.jsx
+++ src/components/OverviewCard.jsx
@@ -1,9 +1,39 @@
-export const OverviewCard =({user,audienceType,audience}) =>{
+import facebookLogo from '../images/icon-facebook.svg'
+import twitterLogo from '../images/icon-twitter.svg'
+import instagramLogo from '../images/icon-instagram.svg'
+import youtubeLogo from '../images/icon-youtube.svg'
+import iconUp from '../images/icon-up.svg'
+import iconDown from '../images/icon-down.svg'
+
+const networkLogos = {
+    Facebook: facebookLogo,
+    Twitter: twitterLogo,
+    Instagram: instagramLogo,
+    YouTube: youtubeLogo
+}
+
+const networkColors = {
+    Facebook: 'bg-facebook',
+    Twitter: 'bg-twitter',
+    Instagram: 'bg-[image:var(--gradient-instagram)]',
+    YouTube: 'bg-youtube'
+}
+
+export const OverviewCard =({user,audienceType,audience,network,isUp,today}) =>{
+    
     return (
-        <article className="bg-light-grayish-blue w-[326px] h-[216px] mb-4 rounded-[5px] mx-auto">
-            <p>{user}</p>
-            <p>{audience}</p>
-            <p>{audienceType}</p>
+        <article className="bg-light-grayish-blue w-[326px] h-[216px] mb-4 rounded-[5px]  overflow-hidden text-center">
+            <div className={`${networkColors[network]} h-[4px] mb-8 `}></div>
+            <div className='flex items-center place-content-center gap-2 '>
+            <img src={networkLogos[network]} alt={`logo ${network}`} />
+            <p className='text-x text-dark-grayish-blue font-bold'>{user}</p>
+            </div>
+            <p className='text-[56px] font-bold text-very-dark-blue '>{audience}</p>
+            <p className='uppercase tracking-[5px] text-dark-grayish-blue-grayish-blue text-xs mb-4'>{audienceType}</p>
+            <div className='flex items-center place-content-center gap-1 '>
+                <img src={isUp ? iconUp : iconDown} alt={isUp ? "Up arrow" : "Down arrow"} />
+                <p className={`text-xs font-bold ${isUp ? 'text-lime-green' : 'text-bright-red'}`}>{today} today </p>
+            </div>
         </article>
     )
 }
--- src/components/OverviewContainer.jsx
+++ src/components/OverviewContainer.jsx
@@ -2,15 +2,21 @@ import data from "../../data/data.json";
 import {OverviewCard} from "./OverviewCard";
 const OverviewContainer = () => {
   return (
-    <section>
+    <section className="border-2 border-red-600 w-[326px] absolute top-[191px] left-0 right-0 mx-auto">
       {
         data.overview.map(object =>
-          <OverviewCard key={object.id}
+          <OverviewCard 
+          key={object.id}
           user={object.user}
           audienceType={object.audienceType}
           audience={object.audience}
+          network={object.network}
+          isUp={object.isUp}
+          today={object.today}
           />
         )
+
+        
       }
     </section>
   )
